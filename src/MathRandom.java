7a09852730fab4926cf66afe936bd834010e328b
KingEvis <evaldas.moisenka@gmail.com>
2021-03-10 16:35:54 +0200

    Newfile - Test class

---
+++ src/MathRandom.java
@@ -0,0 +1,14 @@
+public class MathRandom {
+    public static void main(String[] args) {
+        System.out.println("MathRandom.java exercise");
+        randomGen();
+    }
+    public static void randomGen(){
+        double numA;
+        int numB;
+        int numC;
+
+        //Code goes here
+
+    }
+}
---
+++ src/StringManipulation.java
@@ -0,0 +1,34 @@
+public class StringManipulation {
+    public static void main(String[] args) {
+        System.out.println("StringManipulation.java exercise");
+        System.out.println("getStringSize:");
+        getStringSize("I am a really long string");
+        System.out.println("stringPrinter:");
+        stringPrinter();
+        System.out.println("stringSaw:");
+        stringSaw();
+        System.out.println("parseMe:");
+        parseMe();
+    }
+    public static void getStringSize(String s){
+        //Code here(Find the size of s)
+
+    }
+    public static void stringPrinter(){
+        //FIX the line below
+        //System.out.println("We are "DANCERS", so all we do is "DANCE"!");
+    }
+    public static void stringSaw(){
+        String info = "Tom Jackson 35";//DO NOT CHANGE ME
+        String fName, lName;
+        String age;
+        //Code here
+
+    }
+    public static void parseMe(){
+        String stringA = "30";//DO NOT CHANGE ME
+        String stringB = "15";//DO NOT CHANGE ME
+        //Code here
+    }
+
+}
